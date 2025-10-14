e75a872386c6ade3feeff15eb64aadc9c1e23a0b
vgurza17 <vgurza@centro.edu.mx>
2025-10-13 18:50:52 -0600

    colorpicker

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,8 +1,12 @@
 const themeToggleBtn = document.getElementById("theme-toggle-btn");
+const colorPicker = document.getElementById ("color-picker");
+
 themeToggleBtn.addEventListener ("click", function() {
 document.body.classList.toggle("dark"); 
 });
-
+colorPicker.addEventListener ("change",function(){
+    document.body.style.color= colorPicker.value;
+})
 /*
 setTimeout(() =>) 
     alert("me diste click");   
