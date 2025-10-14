c2fe722e026960eecbc8510e94253b973de0f6a1
vgurza17 <vgurza@centro.edu.mx>
2025-10-13 19:04:54 -0600

    background

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,11 +1,15 @@
 const themeToggleBtn = document.getElementById("theme-toggle-btn");
 const colorPicker = document.getElementById ("color-picker");
+const bgPicker = document.getElementById("bg-picker");
 
 themeToggleBtn.addEventListener ("click", function() {
 document.body.classList.toggle("dark"); 
 });
 colorPicker.addEventListener ("change",function(){
     document.body.style.color= colorPicker.value;
+})
+bgPicker.addEventListener ("change",function(){
+    document.body.style.backgroundColor= bgPicker.value;
 })
 /*
 setTimeout(() =>) 
